918eb7b615f1faed3774944a585db081292a162e
sebacatt <gsebacatt@gmail.com>
2015-08-15 15:11:48 -0400

    a mi no me vengas con cuentos

--- rpueba/settings.py
+++ rpueba/settings.py
@@ -3,7 +3,7 @@ Django settings for rpueba project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.7/topics/settings/
-
+Que pa estas escribiendo?
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
